7fbff9aca90ac46588b8f02a71666a03f21e332f
Soumajit Ghosh <77200987+Soumajit2004@users.noreply.github.com>
2024-05-11 20:06:10 +0530

    navbar updated

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <div className={"bg-base-300"}>
+        <div className={"bg-base-300 hidden md:flex"}>
             <nav className="navbar container mx-auto bg-base-300 h-[10vh]">
                 <div className="flex-1">
                     <Link className="text-base-content font-bold text-xl" href={"/"}>Krishi Gyan</Link>
                 </div>
-              
+                <Link href={"https://agriwelfare.gov.in/"} className={"btn btn-primary"}>krishi portal</Link>
             </nav>
         </div>
     )
